39a1c6063a4f85705e725bfba2e1e32095ab54da
Sergio Morcuende <sergio.morcuende@bq.com>
2015-01-21 11:26:50 +0100

    launch.js updated
    Change-Id: I83857c611e231e8176f18629c25acf51e1f21248

--- src/launch.js
+++ src/launch.js
@@ -5,79 +5,78 @@
 
 'use strict';
 
-var createCustomWin = function(params) {
-    var appList = window.chrome.app.window.getAll();
-    var currentWin = null;
-    var WIDTH = 400,
-        HEIGHT = 300;
-    console.log(appList);
-    for (var i = 0; i < appList.length; i++) {
-        console.log('appInfo', appList[i]);
-        currentWin = appList[i];
-        if (appList[i].id === 'bitbloqSU') {
-            window.console.warn('bitbloqSU is alredy running');
-            return false;
-        }
-    }
-    var _params = {
+var AppWindow = {
+    current: null,
+    defaultConfig: {
         frame: 'none',
         id: 'bitbloqSU',
         resizable: false,
         hidden: false,
         outerBounds: {
-            width: WIDTH,
-            height: HEIGHT,
+            width: 400,
+            height: 300,
             left: 0,
             top: 0
         }
-    };
-    //attributes rewrite
-    for (var param in params) {
-        _params.param = params[param];
-    }
+    },
+    set: function(createdWindow) {
+        this.current = createdWindow;
 
-    window.chrome.app.window.create('index.html', _params);
+        //this.current.resizeTo(200, 200);
+        //this.current.hide();
+        this.current.clearAttention();
 
-    if (!currentWin) {
-        currentWin = window.chrome.app.window.get('bitbloqSU');
-        if(!currentWin){
-            window.setTimeout(createCustomWin, 1000); //try again in 1sec
-            return;
+        this.addWindowListerners();
+    },
+    get: function() {
+        return this.current;
+    },
+    initialize: function(params) {
+        var appList = window.chrome.app.window.getAll();
+
+        for (var i = 0; i < appList.length; i++) {
+            console.log('appInfo', appList[i]);
+
+            if (appList[i].id === 'bitbloqSU') {
+                window.console.warn('bitbloqSU is alredy running');
+                return false;
+            }
+
         }
-        // currentWin.innerWidth = 100;
-        // currentWin.innerHeight = 100;
-        // currentWin.screenX = screen.width;
-        // currentWin.screenY = screen.height;
-        //alwaysLowered = true;
 
-        //currentWin.resizeTo(WIDTH, HEIGHT);
-        currentWin.resizeTo(250, 250);
-        currentWin.onClosed.addListener(function(data) {
+        //attributes rewrite
+        if (params) {
+            for (var param in params) {
+                this.defaultConfig.param = params[param];
+            }
+        }
+
+        window.chrome.app.window.create('index.html', this.defaultConfig, this.set.bind(this));
+
+    },
+    addWindowListerners: function() {
+
+        this.current.onClosed.addListener(function(data) {
             window.console.warn('bitbloqSU closed', data);
-            createCustomWin({
-                'hidden': true
-            });
         });
+
     }
 
 };
 
-
 //Live cycle handler
-
 window.chrome.app.runtime.onLaunched.addListener(function(launchData) {
     window.console.log(launchData);
     if (launchData.id !== 'launcher') {
         window.console.warn('Extension open from chrome extensions page');
     }
-    createCustomWin();
+    AppWindow.initialize();
 });
 
 window.chrome.app.runtime.onRestarted.addListener(function(data) {
     window.console.log('onRestarted event', data);
-    createCustomWin();
+    AppWindow.initialize();
 });
-
 
 window.chrome.runtime.onUpdateAvailable.addListener(function(data) {
     window.console.log('onUpdateAvailable event', data);
