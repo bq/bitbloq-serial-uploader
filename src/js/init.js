e215897f86a83c67419d45f69fcafb3c836d62dc
Sergio Morcuende <sergio.morcuende@bq.com>
2015-02-11 16:03:03 +0100

    [SWBIT-447] bitbloqSU - Serial unit test
    Change-Id: Ieaa34767ee3a07076cfa014bc2a9907a06077c98

--- src/js/init.js
+++ src/js/init.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global bitbloqSU, $ */
+/* global bitbloqSU */
 /* jshint unused:false */
 /* *****************************
 Chrome App interface management
@@ -7,26 +7,3 @@ Chrome App interface management
 if (!window.bitbloqSU) {
     window.bitbloqSU = {};
 }
-
-bitbloqSU.UI = (function() {
-    var appWindow = window.chrome.app.window.current();
-
-    function addDOMListeners() {
-        $('body').on('contextmenu', function() {
-            return false;
-        });
-    }
-
-    var init = function() {
-        console.log('bitbloqSU.UI.init');
-        addDOMListeners();
-    };
-
-    return {
-        addDOMListeners: addDOMListeners,
-        appWindow: appWindow,
-        init: init
-    };
-})();
-
-bitbloqSU.UI.init();
